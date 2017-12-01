8cfd2d698151a80ce362708a5210dd6dc6b327e4
mturcanu <gomacroice@gmail.com>
2017-12-01 15:34:42 +0200

    Add new war

--- src/main/java/utils/Settings.java
+++ src/main/java/utils/Settings.java
@@ -13,7 +13,7 @@ public class Settings {
         public static Connection getConnection() throws SQLException {
             if (connection == null) {
                 System.out.println("PostgreSQL JDBC Driver Registered!");
-                connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-217-232-100.eu-west-1.compute.amazonaws.com:5432/d69lpdvnmq016b?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "abasutwqlhnerx", "064c298d1f9a769459655d5dd456a0bf2bb3ab0600ebb520c7915efbe4e7f3c4");
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/studentapp", "postgres", "postgres");
             }
             return connection;
     }
